f42a6f127d5c1dacdc316888a7a5978a9e8980ed
linushuynh <linusmhuynh@gmail.com>
2023-01-13 18:21:54 -0800

    Updates

---
+++ precache-manifest.0cab9edfeef48183c804b6eb7652d828.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a2b1138b1e18f32a091887d5539dc9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b2957cfb73a7c66058d",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "e4c158d347e3b2d04083",
+    "url": "./static/css/main.08285bb6.chunk.css"
+  },
+  {
+    "revision": "7b2957cfb73a7c66058d",
+    "url": "./static/js/2.9e49da53.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.9e49da53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c158d347e3b2d04083",
+    "url": "./static/js/main.817971bb.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  },
+  {
+    "revision": "6e190eb7b580983ce09c7ccf0c91519d",
+    "url": "./static/media/pikarun.6e190eb7.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/linushuynh/linushuynh.github.io/precache-manifest.cafc837f6696cfcd74a338bb62e41666.js"
+  "./precache-manifest.0cab9edfeef48183c804b6eb7652d828.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/linushuynh/linushuynh.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
