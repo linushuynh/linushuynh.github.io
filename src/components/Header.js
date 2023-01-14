c12ccf25ac4ed99bb24e185359daf9ce607aa4ef
linushuynh <linusmhuynh@gmail.com>
2023-01-13 18:16:56 -0800

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import Typical from "react-typical";
+import Switch from "react-switch";
+import pikarun from "../moreassets/pikarun.gif"
+
+class Header extends Component {
+  titles = [];
+
+  constructor() {
+    super();
+    this.state = { checked: false };
+    this.onThemeSwitchChange = this.onThemeSwitchChange.bind(this);
+  }
+
+  onThemeSwitchChange(checked) {
+    this.setState({ checked });
+    this.setTheme();
+  }
+
+  setTheme() {
+    var dataThemeAttribute = "data-theme";
+    var body = document.body;
+    var newTheme =
+      body.getAttribute(dataThemeAttribute) === "dark" ? "light" : "dark";
+    body.setAttribute(dataThemeAttribute, newTheme);
+  }
+
+  render() {
+    if (this.props.sharedData) {
+      var name = this.props.sharedData.name;
+      this.titles = this.props.sharedData.titles.map(x => [ x.toUpperCase(), 1500 ] ).flat();
+    }
+
+    const HeaderTitleTypeAnimation = React.memo( () => {
+      return <Typical className="title-styles" steps={this.titles} loop={50} />
+    }, (props, prevProp) => true);
+
+    return (
+      <header id="home" style={{ height: window.innerHeight - 140, display: 'block' }}>
+        <div className="row aligner" style={{height: '100%'}}>
+          <div className="col-md-12">
+            <div>
+              <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
+              <br/>
+              <h1 className="mb-0">
+                <Typical steps={[name]} wrapper="p" />
+              </h1>
+              <div className="title-container">
+                <HeaderTitleTypeAnimation />
+              </div>
+              <Switch
+                checked={this.state.checked}
+                onChange={this.onThemeSwitchChange}
+                offColor="#41B46C"
+                onColor="#353535"
+                className="react-switch mx-auto"
+                width={90}
+                height={40}
+                uncheckedIcon={
+                  // <span
+                  //   className="iconify"
+                  //   // data-icon="twemoji:owl"
+                  //   data-inline="false"
+                  //   style={{
+                  //     display: "block",
+                  //     height: "100%",
+                  //     fontSize: 25,
+                  //     textAlign: "end",
+                  //     marginLeft: "20px",
+                  //     color: "#353239",
+                  //   }}
+                  // ></span>
+                  <img
+                  alt="pikarun"
+                  className="iconify"
+                  data-inline="false"
+                  style={{
+                        display: "flex",
+                        alignSelf: "flex-end",
+                        height: "30px",
+                        width: "35px",
+                        fontSize: 12,
+                        paddingTop: "5px",
+                        textAlign: "end",
+                        marginLeft: "5px",
+                        color: "#353239",
+                      }}
+                  src={pikarun}
+                >
+                </img>
+                }
+                checkedIcon={
+                  // <span
+                  //   className="iconify"
+                  //   // data-icon="noto-v1:sun-with-face"
+                  //   data-inline="false"
+                  //   style={{
+                  //     display: "block",
+                  //     height: "100%",
+                  //     fontSize: 25,
+                  //     textAlign: "end",
+                  //     marginLeft: "10px",
+                  //     color: "#353239",
+                  //   }}
+                  // ></span>
+                  <img
+                    alt="pikarun"
+                    className="iconify"
+                    data-inline="false"
+                    style={{
+                      display: "flex",
+                      alignSelf: "flex-end",
+                      height: "30px",
+                      width: "35px",
+                      fontSize: 12,
+                      paddingTop: "5px",
+                      textAlign: "end",
+                      marginLeft: "11px",
+                      color: "#353239",
+                      }}
+                    src={pikarun}
+                  >
+                  </img>
+                }
+                id="icon-switch"
+              />
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
